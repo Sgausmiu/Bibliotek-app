2f744a6313994988734b24ad9598d6aeef9f2f65
Sgausmiu <79138319+Sgausmiu@users.noreply.github.com>
2023-01-25 19:48:36 +0400

    Добавил файлы в конфиг

---
+++ src/main/java/ru/samara/bibliotek/config/BibliotekDispatcherServletInitializer.java
@@ -0,0 +1,51 @@
+package ru.samara.bibliotek.config;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import java.util.EnumSet;
+
+public class BibliotekDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+        registerHiddenFieldFilter(servletContext);
+        registerCharacterEncodingFilter(servletContext);
+    }
+
+    private void registerHiddenFieldFilter (ServletContext Context) {
+        Context.addFilter("hiddenHttpMethodFilter", new HiddenHttpMethodFilter()).addMappingForUrlPatterns(
+                null, true, "/*");
+    }
+
+    private void registerCharacterEncodingFilter (ServletContext Context) {
+        EnumSet<DispatcherType> dispatcherType = EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD);
+
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+
+        FilterRegistration.Dynamic characterEncoding = Context.addFilter("characterEncoding", characterEncodingFilter);
+        characterEncoding.addMappingForUrlPatterns(dispatcherType, true, "/*");
+    }
+}
